d676b99dad1a2126e6568dceb2faec7813af0dcd
Yiff <yiffyang@gmail.com>
2023-08-12 19:24:24 -0400

    this is for jubensha and git test,and it's for git test, 3rd commit

--- other/gittest.java
+++ other/gittest.java
@@ -2,6 +2,7 @@ public class gittest {
     public static void main(String[] args) {
         System.out.println("this is for git test");
         System.out.println("hello git");
+        System.out.println("third test");
 
     }
 }
